6e28b2851f8e5bb905b4aafe007773469d8465a6
Kertayx <159715413+KornerAlex@users.noreply.github.com>
2026-05-07 23:02:40 +0200

    genetic algorithm basic usable __init__

--- ga/genetic_algorithm.py
+++ ga/genetic_algorithm.py
@@ -1,21 +1,48 @@
 from ga.population import Population
 from ga.individual import Individual
+from ga.config import GAConfig
+from ga.selection.base import SelectionStrategy
+from ga.crossover.base import CrossoverStrategy
+from ga.mutation.base import MutationStrategy
+from ga.replacement.base import ReplacementStrategy
+from ga.evaluator import Evaluator
+from ga.generator import Generator
+
+import numpy as np
+
 
 
 class GeneticAlgorithm:
 
     def __init__(
         self,
-        population_size,
-        genome_generator,
-        selection,
-        crossover,
-        mutation,
-        replacement,
-        evaluator,
+        config : GAConfig,
+        genome_generator : Generator,
+        selection : SelectionStrategy,
+        crossover : CrossoverStrategy,
+        mutation : MutationStrategy,
+        replacement : ReplacementStrategy,
+        evaluator : Evaluator,
+        rng : np.random.Generator,
     ):
+        if not isinstance(config, GAConfig):
+            raise TypeError(f"config must be instance of GAConfig, got {type(config)}")
+        if not isinstance(self.genome_generator, Generator):
+            raise TypeError(f"genome_generator must be instance of Generator, got {type(genome_generator)}")
+        if not isinstance(selection, SelectionStrategy):
+            raise TypeError(f"selection must be instance of Selection, got {type(selection)}")
+        if not isinstance(crossover, CrossoverStrategy):
+            raise TypeError(f"crossover must be instance of Crossover, got {type(crossover)}")
+        if not isinstance(mutation, MutationStrategy):
+            raise TypeError(f"mutation must be instance of Mutation, got {type(mutation)}")
+        if not isinstance(replacement, ReplacementStrategy):
+            raise TypeError(f"replacement must be instance of Replacement, got {type(replacement)}")
+        if not isinstance(evaluator, Evaluator):
+            raise TypeError(f"evaluator must be instance of Evaluator, got {type(evaluator)}")
+        if not isinstance(rng, np.random.Generator):
+            raise TypeError(f"evaluator must be instance of Evaluator, got {type(rng)}")
 
-        self.population_size = population_size
+        self.config = config
 
         self.selection = selection
         self.crossover = crossover
@@ -23,10 +50,12 @@ class GeneticAlgorithm:
         self.replacement = replacement
         self.evaluator = evaluator
 
+        self.rng = rng
+
         self.population = Population(
             [
                 Individual(genome_generator())
-                for _ in range(population_size)
+                for _ in range(self.config.population_size)
             ]
         )
 
